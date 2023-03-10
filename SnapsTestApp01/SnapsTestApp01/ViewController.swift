97efd71c34f49347e30a94f94b183da256911c08
anthonySnaps <anthony.lee@snaps.com>
2023-03-10 13:59:43 +0900

    ui 개발

--- SnapsTestApp01/SnapsTestApp01/ViewController.swift
+++ SnapsTestApp01/SnapsTestApp01/ViewController.swift
@@ -6,12 +6,21 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let helloText = UILabel()
+        helloText.text = "hello test app"
+        self.view.addSubview(helloText)
+        helloText.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        self.view.backgroundColor = .yellow
     }
 
 
